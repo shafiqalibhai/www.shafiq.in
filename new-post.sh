8e8467c0dbf936f1be513d2c5001a39d5442311f
Shafiq Alibhai <shafiq@deployview.com>
2025-09-28 07:08:56 +0000

    adds mobile hugo

--- new-post.sh
+++ new-post.sh
@@ -1,4 +1,4 @@
 set -x
 
 # docker run --rm -it -v $PWD:/src klakegg/hugo:ubuntu new content\/posts\/$(date +%Y)/$(date +%G-%m-%d-%H%M).md
-./hugo new posts\/$(date +%Y)/$(date +%G-%m-%d-%H%M%S).md
+./hugo-mobile new posts\/$(date +%Y)/$(date +%G-%m-%d-%H%M%S).md
